ddf9f99711cdd26acc4082d75e8ea6c57260ab08
Wuhib <wuhebsherif@gmail.com>
2021-08-14 11:12:37 +0300

    pharmacist search patient function modified

--- patient/models.py
+++ patient/models.py
@@ -88,7 +88,7 @@ class XrayExamination(models.Model):
     x_ray_image = models.ImageField(null=True)
     x_ray_report = models.TextField(null=True)
     impressions = models.TextField(null=True)
-    reported_by = models.ForeignKey(Staff, on_delete=models.CASCADE, related_name='xray_reported_by')
+    requested_to = models.ForeignKey(Staff, on_delete=models.CASCADE, related_name='xray_reported_to')
     date_of_report = models.DateTimeField(null=True)
     status = models.CharField(max_length=50, default='pending')
 
