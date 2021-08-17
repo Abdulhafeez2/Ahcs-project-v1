85496f597bbf296ce69a53bbf765b60408d3eb96
Abdulhafeez2 <abdulhafeezkemal@gmail.com>
2021-08-17 02:54:47 -0700

    appointment form

--- radiologist/urls.py
+++ radiologist/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.radiologist_homepage, name='radiologist_homepage_url'),
     path('request_detail/<str:pk>/', views.request_detail, name='request_detail_url'),
-    path('remove_from_list/<str:pk>/', views.remove_from_list, name='remove_from_list_url'),
+    path('remove_from_list/<str:pk>/', views.remove_from_list, name='remove_from_radiology_list_url'),
 
 ]
